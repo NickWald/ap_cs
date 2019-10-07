08be8cedb3a157a5bc9f0cfdf01da1e887c02ea2
NickWald <nicholas.wald@gmail.com>
2019-10-07 17:18:21 -0400

    did loops practice in class

---
+++ in_class/LOOPS/loops.java
@@ -0,0 +1,28 @@
+//PRINT A STRING VERTICALLY:
+public class loops{
+  public static void main(String[] args){
+    System.out.println()
+  }
+
+public void printCharsFor(String s){
+  for(int i = 0; i <= s.length(); i++){ //use i for iterator var
+    println(s.charAt(i));
+  }
+}
+
+
+//or as a while loop:
+public void printCharsWhile(String s){
+  while int i = 0(i < s.lenth){
+    println(s.charAt(i));
+    x++;
+  }
+}
+
+//NOW BACKWARDS
+public void printCharsBackward(String s){
+  for(int i = s.length()-1; i >= 0; i--){
+    println(s.charAt(i));
+  }
+}
+}
