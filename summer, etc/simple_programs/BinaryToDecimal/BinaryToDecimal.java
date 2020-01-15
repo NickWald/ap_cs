ee9db49c84c65a9a69291915700d0874dcf2913f
NickWald <nicholas.wald@gmail.com>
2020-01-15 17:49:13 -0500

    nsdjsdnfjsdnf

--- summer, etc/simple_programs/BinaryToDecimal/BinaryToDecimal.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class BinaryToDecimal {
-    public static void main(String args[]){
-       Scanner input = new Scanner( System.in );
-       System.out.print("Enter a binary number: ");
-       String binaryString =input.nextLine();
-       System.out.println("Decimal: "+Integer.parseInt(binaryString,2));
-    }
-}
